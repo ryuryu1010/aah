3bd5f0f93a0e6270cf2ca7f99e053b3c37d2121a
ryuryu1010 <211015@std.hi-joho.ac.jp>
2024-05-17 14:50:36 +0900

    ログイン機能（emprole画面に出ない）他終了

--- kadai_0/urls.py
+++ kadai_0/urls.py
@@ -1,5 +1,11 @@
+# employee_management/urls.py
+
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('', views.login, name='login'),
+    path('', views.login_view, name='login'),
+    path('admin/home/', views.admin_home, name='admin_home'),
+    path('reception/home/', views.reception_home, name='reception_home'),
+    path('doctor/home/', views.doctor_home, name='doctor_home'),
 ]
--- kadai_0/views.py
+++ kadai_0/views.py
@@ -1,14 +1,35 @@
-from django.shortcuts import render
+# employee_management/views.py
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login
 
 
-def login(request):
+def login_view(request):
     if request.method == 'GET':
-        return render(request,'login/login.html')
+        return render(request, 'login/login.html')
+
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            if user.emprole == 0:
+                return redirect('admin_home')
+            elif user.emprole == 1:
+                return redirect('reception_home')
+            elif user.emprole == 2:
+                return redirect('doctor_home')
+        return render(request, 'login/login.html', {'error': 'Invalid credentials'})
 
-        if password == 'password':
-            return render(request,'index/a_index.html')
-        else:
-            return render(request,'login/login.html')
+
+def admin_home(request):
+    return render(request, 'index/a_index.html')
+
+
+def reception_home(request):
+    return render(request, 'index/r_index.html')
+
+
+def doctor_home(request):
+    return render(request, 'index/d_index.html')
